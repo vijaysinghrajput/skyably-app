c9bf4255a982f10d917b95a57aa30c4f19da352f
skyably <admin@skyably.com>
2021-01-29 19:02:07 +0530

    url

--- src/URL.js
+++ src/URL.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-export default URL = 'https://skyablypropertygroup.com/admin';
-=======
+
 export default  URL = 'https://skyably.com/skyably-admin';
->>>>>>> cfbc8cc522c7ee066c34f8941cb6574a4a803543
+
